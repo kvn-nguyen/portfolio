d8290cef8c9d6eff3e5f3ccd948518b78852f5ee
Kevin Nguyen <kevisaann@gmail.com>
2022-09-01 13:58:39 -0700

    updated logo size

--- components/Header.js
+++ components/Header.js
@@ -12,8 +12,8 @@ const Header = () => {
           <img
             src="/static/kn_logo.jpg"
             alt="logo pic"
-            width="50px"
-            height="50px"
+            width="40px"
+            height="40px"
           />
         </a>
       </div>
